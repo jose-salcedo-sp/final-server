020b0bed5ae72972530797d45c8468bf943959f4
Emiliano Hinojosa <emiliano.hinojosa427@gmail.com>
2025-05-12 22:54:40 -0600

    Database connection

--- data_server/data_server.c
+++ data_server/data_server.c
@@ -1,5 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
 
-int main(){
-	printf("Hello World\n");
+int main() {
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+
+    const char *server = "172.21.16.1";
+    const char *user = "admin";
+    const char *password = "UP2025";
+    const char *database = "MessengerDatabase";
+
+    conn = mysql_init(NULL);
+
+    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)) {
+        fprintf(stderr, "Connection failed: %s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    if (mysql_query(conn, "SHOW TABLES")) {
+        fprintf(stderr, "Query failed: %s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    res = mysql_store_result(conn);
+
+    printf("Tables:\n");
+    while ((row = mysql_fetch_row(res)) != NULL) {
+        printf("%s\n", row[0]);
+    }
+
+    mysql_free_result(res);
+    mysql_close(conn);
+
+    return 0;
 }
+
